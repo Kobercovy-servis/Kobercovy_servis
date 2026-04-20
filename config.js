66ae0973fa6271da5aaf1b79ad330a9b1eda6b1a
michalplicka2-art <michal.plicka2@gmail.com>
2026-04-20 16:03:13 +0200

    Refactor APP_CONFIG structure in config.js

--- config.js
+++ config.js
@@ -1,21 +1,10 @@
-// config.js
 window.APP_CONFIG = {
-  // Formspree endpoint (doplň svůj reálný)
   FORMSPREE_ACTION: "https://formspree.io/f/xqedanbl",
-
-  // Google Maps API key (až budeš mapu napojovat)
   GOOGLE_MAPS_API_KEY: "AIzaSyCLw_AH4X1T0YisInKWoBcHcVNkivDrqdY",
-
-  // Data pro sběrná místa (CSV/JSON url) – zatím placeholder
   PLACES_DATA_URL: "https://docs.google.com/spreadsheets/d/1CJYobC4NWdUv03alqpUYyte--Fw-tiRilqZuYzE-87g/export?format=csv",
-,
-",
-
-  // Ceník
   PRICE_CLEAN_PER_M2: 300,
   PRICE_EDGE_PER_M: 99,
   PRICE_IMP_PER_M2: 40
 };
-
 
 
